902849c1246bb5b9574efe5a1b16716778c56fab
bkinjal <kinjalpatel2005@yahoo.com>
2015-06-20 11:59:22 -0500

    Added Naming convention
    Added Naming convention

---
+++ NamingCoventions.java
@@ -0,0 +1,29 @@
+package com.digitek.test;
+
+public class NamingCoventions {
+
+	public static void main(String[] args) {
+		addision();
+
+	}
+
+	// PROJECTS, CLASSES, METHODS, VARIABLES
+
+	// This is how we creat project right clickpackage explorer, new,java
+	// project ,give name to project.
+	// Then we creat a package right click project,give name as reqiverment
+	// application starts with com.
+	// Then we creat a class right click package, class, enter class name which
+	// you will work on.
+	// This is how we right Method
+	public static void addision() {
+		// This is how we right Variables
+		int number1 = 33;
+		int number2 = 14;
+
+		// This is how we right Block of code
+		int sum = number1 + number2;
+		System.out.println("sum " + sum);
+		// Now we can exicute the condition for code result
+	}
+}
